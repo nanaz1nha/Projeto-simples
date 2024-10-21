6d8546161c7150265ff8ff72cfca309b64228b86
nanaz1nha <thaina_olliveira@hotmail.com>
2024-10-21 09:35:17 -0300

    criando o sql em aula

---
+++ database/cadastro.sql
@@ -0,0 +1,17 @@
+
+DROP DATABASE IF EXISTS cadastro;
+
+CREATE DATABASE cadastro
+	CHARACTER SET utf8mb4
+    COLLATE utf8mb4_general_ci;
+
+USE cadastro;
+
+CREATE TABLE usuario (
+
+    usuario_nome VARCHAR(127) NOT NULL,
+    usuario_numero VARCHAR(255) NOT NULL,
+ 
+)
+
+
